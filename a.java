6e8800cd10300535b225ad64be28077a1972dde4
kanjita bhavsar <kanjita.softyoi@gmail.com>
2022-04-09 20:13:29 +0530

    added new file

---
+++ a.java
@@ -0,0 +1,16 @@
+class a
+ {
+    public static int firstM(int x,int y)
+{
+System.out.println(x+y);
+//return x+y;
+}
+
+public static void main(String args[])
+{
+      int s;
+      s=firstM(12,23);
+      //System.out.println(firstM());
+}
+}
+}
