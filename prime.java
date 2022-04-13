9aa2b3267edc6de58186c756a4653241dc961dca
kanjita bhavsar <kanjita.softyoi@gmail.com>
2022-04-13 16:47:18 +0530

    Added prime number program

---
+++ prime.java
@@ -0,0 +1,17 @@
+class prime
+{
+ public static void main(String args[])
+ {
+     int n=3;
+     
+   if(n % 2 == 0)
+   {
+    System.out.println("not prime");
+	n++;
+   }
+   else
+   {
+    System.out.println("prime");
+	}
+ }
+}
